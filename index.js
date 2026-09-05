e40d0b6cb2fc9aeb3b4c54fae0400c98f898436a
Aditya Raghav <adityaraghav500@gmail.com>
2026-09-05 21:55:43 +0530

    Add form submission handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
 
 
 setTimeout(function(){
@@ -14,5 +15,25 @@ modalCloseBtn.addEventListener("click", function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log("Form submitted")
+    modalText.innerHTML= `
+            <div class="modal-inner-loading">
+                <img src="images/loading.svg" class="loading">
+                <p id="upload-text">
+                Uploading your data to the dark web...
+                </p>
+            </div>
+                `
+    
+        setTimeout(function(){
+            document.getElementById("upload-text").textContent = "Making the sale..."
+        }, 1500)
+
+        setTimeout(function(){
+            document.getElementById("modal-inner").innerHTML= `<h2>Thanks you sucker! </h2>
+                                    <p>We just sold the rights to your eternal soul.</p>
+                                    <div class="idiot-gif">
+                                        <img src="images/pirate.gif">
+                                    </div>
+    ` 
+        }, 3000)
 })
