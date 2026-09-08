858ac4df5726a89a417eb8a5ccdab498188deda3
Aditya Raghav <adityaraghav500@gmail.com>
2026-09-08 16:29:51 +0530

    Swap accept and decline buttons on hover

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
 const modalText = document.getElementById("modal-text")
+const declineBtn = document.getElementById("decline-btn")
+const modalChoiceBtns = document.getElementById("modal-choice-btns")
 
 
 setTimeout(function(){
@@ -11,6 +13,10 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener("click", function(){
     modal.style.display = "none"
+})
+
+declineBtn.addEventListener('mouseover', function(){
+    modalChoiceBtns.classList.toggle("reverse")
 })
 
 consentForm.addEventListener('submit', function(e){
